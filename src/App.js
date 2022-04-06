e67923a6a25c303d2cd03a95466fde2c68b5bdc3
JoshJAL <JoshuaALevine3@gmail.com>
2022-04-06 02:16:56 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import { AnimatePresence } from "framer-motion";
+import GlobalStyle from "./globalStyles";
+import styled from "styled-components";
+
+const Section = styled.section`
+  overflow-x: hidden;
+  overflow-y: hidden;
+`;
 
 function App() {
+  let location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Section>
+      <GlobalStyle />
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </AnimatePresence>
+    </Section>
   );
 }
 
